d9c3d5a4c03fa8222065e8d4dccec0e723cd9edb
I.Safonov <safonovil17@yandex.ru>
2022-10-17 12:31:48 +0300

    delete console log

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-function Header() {
+function Header(props) {
     return (
         <header>
             <div className="headerLeft">
@@ -9,7 +9,7 @@ function Header() {
                 </div>
             </div>
             <ul className="headerRight">
-                <li>
+                <li onClick={props.onClickCart}>
                     <img src="/img/cart.svg" alt="" />
                     <span>1205 руб.</span>
                 </li>
